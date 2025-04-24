125227c21cc875ff73aa611d690b7feada0f8db3
Faisal Azzari <fac12104@gmail.com>
2025-04-24 21:14:08 +0700

    AuthForm

--- tubes-ippl/src/App.js
+++ tubes-ippl/src/App.js
@@ -1,11 +1,14 @@
 import './components/HomePage/HomePage'
+import './components/AuthForm/AuthForm'
 import './App.css';
 import HomePage from './components/HomePage/HomePage';
+import AuthForm from './components/AuthForm/AuthForm';
 
 function App() {
   return (
     <div className="App">
       <HomePage/>
+      <AuthForm/>
     </div>
   );
 }
